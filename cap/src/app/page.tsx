37ea6243a26e3dfd711d97401e7c2b66136225a0
Lane Meeks <147874358+lanemeeks@users.noreply.github.com>
2025-02-22 16:15:47 -0600

    adding chat implementation

--- cap/src/app/page.tsx
+++ cap/src/app/page.tsx
@@ -1,6 +1,12 @@
 import Image from "next/image";
+import Kommunicate from "@kommunicate/kommunicate-chatbot-plugin";
+import KommunicateChat from './chat'
 
 export default function Home() {
+  Kommunicate.init("12b433f074c24d6b911d3dbc2509ab1", {
+    automaticChatOpenOnNavigation: true,
+    popupWidget: true,
+  });
   return (
     <h1>Welcome to Home Page</h1>
   );
